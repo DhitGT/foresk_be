ee415918b3de197ca900f50c76ec99d99a6ca0f7
muhamad adli <adli01@gmail.com>
2025-02-18 10:23:14 +0700

    push

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\eskul;
 use App\Models\EskulKas;
 use App\Models\EskulMember;
+use App\Models\instansi;
+use App\Models\user;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class ChartController extends Controller
@@ -14,14 +17,18 @@ class ChartController extends Controller
     {
         try {
             // Fetch EskulKas with related Eskul
+
+            $user = Auth::user();
+            $instansi = instansi::where('owner_id', $user->id)->first();
+
             $kas = EskulKas::with([
                 'eskul' => function ($query) {
                     $query->select("*");
                 }
-            ])->get();
+            ])->where('instansi_id', $instansi->id)->get();
 
             // Fetch EskulMember with Eskul and count members for each Eskul
-            $member = eskul::withCount('eskulMembers')->get();
+            $member = eskul::withCount('eskulMembers')->where('instansi_id', $instansi->id)->get();
 
             return response()->json(['kas' => $kas, 'member' => $member]);
         } catch (\Throwable $th) {
